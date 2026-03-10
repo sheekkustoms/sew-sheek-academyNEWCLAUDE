b979458c07c03ee199017c59035acbaaeb4bc6bb
Sheek 24 <sheek24kustoms@gmail.com>
2026-03-10 04:18:49 +0000

    File changes

--- src/pages/Community.jsx
+++ src/pages/Community.jsx
@@ -1,6 +1,7 @@
-import React, { useState } from "react";
+import React, { useState, useCallback } from "react";
 import { base44 } from "@/api/base44Client";
 import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
+import PullToRefresh from "../components/shared/PullToRefresh";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
